3946c586b9ee340360a35f73e530aeb769c595d9
Alex Bush <alex.v.bush@gmail.com>
2017-10-06 17:07:13 -0700

    Add failing tags controller spec

--- Tests/AppTests/TagsControllerSpec.swift
+++ Tests/AppTests/TagsControllerSpec.swift
@@ -1,5 +1,14 @@
-
+import XCTest
 import Testing
 import Quick
 import Nimble
 
+class TagsControllerSpec: QuickSpec {
+    override func spec() {
+        describe("something") {
+            it("does somehting") {
+                expect(true).toNot(beTrue())
+            }
+        }
+    }
+}
