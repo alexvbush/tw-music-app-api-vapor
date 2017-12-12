515e5ef72fd8842c8a3dfe4508096cb78e0da85c
Alex Bush <alex.v.bush@gmail.com>
2017-12-12 17:01:00 -0500

    Remove unnecessary controller and model

--- Sources/App/Routes.swift
+++ Sources/App/Routes.swift
@@ -3,20 +3,6 @@ import Vapor
 extension Droplet {
     func setupRoutes() throws {
         
-        get("plaintext") { req in
-            return "Hello, world!"
-        }
-
-        // response to requests to /info domain
-        // with a description of the request
-        get("info") { req in
-            return req.description
-        }
-
-        get("description") { req in return req.description }
-        
-        try resource("posts", PostController.self)
-        
         let categoriesStorage = CategoriesStorage()
         let categoriesController = CategoriesController(categoriesStorage: categoriesStorage)
         grouped("api").grouped("v1").get("categories", handler: categoriesController.index)
@@ -24,7 +10,6 @@ extension Droplet {
         let subcategoriesStorage = SubcategoriesStorage()
         let subcategoriesController = SubcategoriesController(subcategoriesStorage)
         grouped("api").grouped("v1").get("categories", String.parameter, "subcategories",
-                                         handler: subcategoriesController.index)
-        
+                                         handler: subcategoriesController.index)        
     }
 }
